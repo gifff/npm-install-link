887cf8ad98ee2963317d3d0acc3c3da20bb44792
Giff Song <giff.song@gmail.com>
2015-08-07 16:21:25 +0900

    Initial commit

---
+++ install.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var path = require('path');
+var execSync = require('child_process').execSync;
+var findup = require('findup-sync');
+
+var pkgpath = findup('package.json');
+var pkg = require(pkgpath);
+
+var links = pkg.links || [];
+
+module.exports = links.map(function (link) {
+
+  // execute npm install in link directory
+  var abspath = path.join(pkgpath, link);
+  var installcmd = 'npm install --production';
+
+  console.log(abspath + '$', installcmd);
+  execSync(installcmd, {
+    cwd: abspath
+  });
+
+  // Link
+  var linkcmd = 'npm link ' + link;
+
+  console.log(pkgpath + '$', linkcmd);
+  execSync(linkcmd, {
+    cwd: pkgpath
+  });
+
+  return abspath;
+});
